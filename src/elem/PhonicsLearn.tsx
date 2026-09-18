10302f5c784c6163d9faa04a22135fc25c75c113
yosepsalang-droid <yosepsalang@gmail.com>
2026-09-18 15:26:00 +0900

    Fix import typo

--- src/elem/PhonicsLearn.tsx
+++ src/elem/PhonicsLearn.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-iimport { PHONICS_DB } from '../data/phonics_db';
-
+import { PHONICS_DB } from '../data/phonics_db';
 interface PhonicsLearnProps {
   day: number; 
   onBack: () => void;
